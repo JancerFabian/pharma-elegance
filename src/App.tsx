1900bd7f56fcdb4dfd28674033b027f24ec2cca4
Jancer Fabian Rodriguez <jancer.fabian@gmail.com>
2026-02-17 15:37:36 -0400

    fix base path for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <WhatsAppButton />
-      <BrowserRouter basename="/pharma-elegance">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
